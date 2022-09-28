0b0b89c8b3a9e59d87720abbf59c0862f1ef928d
Matthew <mfarley3573@bths.edu>
2022-09-27 22:33:39 -0400

    Added two new classes (Poem & ConstructionPricer.) Poem writes a short poem while ConstructionPricer calculates the costs of lumber & windows with and without tax with given details. Added a way for the user to input amount of building materials (lumber & windows) and tax rate. Will also display the costs of lumber & windows with and without tax.

--- src/U2L2Runner.java
+++ src/U2L2Runner.java
@@ -1,16 +1,32 @@
+import java.util.Scanner;
 public class U2L2Runner {
-    public static void main(String[] args) {
-        Rectangle a = new Rectangle(150, 200);
-        Rectangle b = new Rectangle(100);
-        Rectangle c = new Rectangle();
-        a.setWidth(125);
-        b.setLength(125);
-        b.setWidth(125);
-        c.setWidth(125);
-        int totalFencing = a.calculatePerimeter() + b.calculatePerimeter() + c.calculatePerimeter();
-        System.out.println("You will need " + totalFencing + " feet of fencing.");
-        int totalSeeding = a.calculateArea() + b.calculateArea() + c.calculateArea();
-        System.out.println("You will need " + totalSeeding + " sq. feet of seeding.");
 
+    public static void main(String[] args) {
+        Poem poem = new Poem("Adrian");
+        poem.printPoem();
+        //Scanner s = new Scanner(System.in);
+        //System.out.println("How many pieces of lumber do you need for the home you are building? ");
+        //int totalLumber = s.nextInt();
+        //System.out.println("How many windows do you need? ");
+        //int totalWindows = s.nextInt();
+        //System.out.print("What is the tax rate for your area? ");
+        //double tax = s.nextDouble();
+        //ConstructionPricer a = new ConstructionPricer(11.5, 25.75,tax);
+        //double cost = a.materialsCost(totalLumber,totalWindows);
+        //System.out.println("Total Material Costs: $" + cost);
+        //double finalCost = a.totalWithTax(cost);
+        //System.out.print("Grand Total after Tax: $");
+        //System.out.format("%.2f%n", finalCost);
+        //Rectangle a = new Rectangle(150, 200);
+        //Rectangle b = new Rectangle(100);
+        //Rectangle c = new Rectangle();
+        //a.setWidth(125);
+        //b.setLength(125);
+        //b.setWidth(125);
+        //c.setWidth(125);
+        //int totalFencing = a.calculatePerimeter() + b.calculatePerimeter() + c.calculatePerimeter();
+        //System.out.println("You will need " + totalFencing + " feet of fencing.");
+        //int totalSeeding = a.calculateArea() + b.calculateArea() + c.calculateArea();
+        //System.out.println("You will need " + totalSeeding + " sq. feet of seeding.");
     }
 }
